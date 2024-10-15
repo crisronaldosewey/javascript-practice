5430e6e726600831c69af6895888a096f526d40d
crisronaldosewey <henry.hernandez@stu.vertexacademies.org>
2024-10-15 14:04:53 -0400

    Add files via upload

--- Henry_Hernandez_JS_00.js
+++ Henry_Hernandez_JS_00.js
@@ -1,1 +1,6 @@
-console.log("Hello, Javier!");
+console.log("Hello, World!")
+console.log("¡Hola, Mundo!")
+console.log("Hello,")
+console.log("World!")
+console.log("Hello,\nWorld!")
+console.log("The computer says, \"Hello World\"");
