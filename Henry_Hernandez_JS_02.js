c449a8572ce9d287cb1c2d7d4dc3e25630485268
crisronaldosewey <henry.hernandez@stu.vertexacademies.org>
2024-10-16 14:10:33 -0400

    Add files via upload

---
+++ Henry_Hernandez_JS_02.js
@@ -0,0 +1,14 @@
+// Example:             // Expected Output
+console.log(4 + 2.5)   // 6.5
+
+// Write 5 more expressions below:
+console.log(6/3) // Expected Output: 2
+  console.log(9*3) // Expected Output: 27
+    console.log(9%2) // Expected Output: 1
+      console.log(2+2) // Expected Output: 4
+        console.log(4**3) // Expected Output: 64
+        
+// Boolean work
+console.log(!true)
+console.log(true||true)
+console.log(false||true)
